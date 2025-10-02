9cdd3f3af48b9a557fa2e459bb3a4448fa5910e9
zlatakimjeon <zlatakimjeon@gmail.com>
2025-10-02 13:47:23 +0300

    128

--- test/test_relation.c
+++ test/test_relation.c
@@ -49,10 +49,33 @@ void test_duplicates() {
     free_relation(res);
 }
 
+void test_realloc_success() {
+    printf("Testing successful realloc...\n");
+    
+    struct Pair rp[] = {{1, 2}, {3, 4}, {5, 6}, {7, 8}};
+    struct Pair sp[] = {{2, 10}, {4, 20}, {6, 30}, {8, 40}};
+    struct Relation r = {rp, 4};
+    struct Relation s = {sp, 4};
+
+    struct Relation *res = join_binary(&r, &s);
+    assert(res != NULL);
+    assert(res->size == 4);
+    
+    int expected[][2] = {{1, 10}, {3, 20}, {5, 30}, {7, 40}};
+    for (unsigned i = 0; i < res->size; i++) {
+        assert(res->pairs[i].first == expected[i][0]);
+        assert(res->pairs[i].second == expected[i][1]);
+    }
+    
+    free_relation(res);
+    printf("Realloc success test passed!\n");
+}
+
 int run_all_tests(void) {
     test_empty_relations();
     test_single_join();
     test_no_join();
     test_duplicates();
+    test_realloc_success();
 	return 0;
 }                           
